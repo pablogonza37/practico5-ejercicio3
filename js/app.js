266a3be32dd97901ee03f52a73e54a41a3b8d552
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2023-11-26 14:18:37 -0300

    se agrego funcionalidad y se hizo responsive

---
+++ js/app.js
@@ -0,0 +1,26 @@
+function agregarTarea() {
+    const tarea = document.getElementById("ingresoTarea").value;
+
+    if (tarea.trim() === "") {
+      alert("Por favor, ingrese una tarea.");
+      return;
+    }
+
+    const lista = document.getElementById("listaTareas");
+
+    const nuevaTarea = document.createElement("li");
+    nuevaTarea.className = "list-group-item d-flex justify-content-between align-items-center";
+    nuevaTarea.innerHTML = `
+      ${tarea}
+      <button class="btn btn-danger btn-sm" onclick="eliminarTarea(this)">Eliminar</button>
+    `;
+
+    lista.appendChild(nuevaTarea);
+
+    document.getElementById("ingresoTarea").value = "";
+  }
+
+  function eliminarTarea(elemento) {
+    const tarea = elemento.parentNode;
+    tarea.parentNode.removeChild(tarea);
+  }
