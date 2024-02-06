02c0d57faf15918bf4939cc33795bf6c765c443f
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2024-02-06 13:34:46 -0300

    se realizo la validacion del formulario

--- js/app.js
+++ js/app.js
@@ -1,31 +1,21 @@
-function agregarTarea() {
-    const tarea = document.getElementById("ingresoTarea").value;
+function agregarTarea(event) {
+  event.preventDefault();
+  const tarea = document.getElementById("ingresoTarea").value;
+  const lista = document.getElementById("listaTareas");
+  const nuevaTarea = document.createElement("li");
+  nuevaTarea.className =
+    "list-group-item d-flex justify-content-between align-items-center";
+  nuevaTarea.innerHTML = `
+    ${tarea}
+    <button class="btn btn-danger btn-sm" onclick="eliminarTarea(this)">Eliminar</button>
+  `;
 
-    if (tarea.trim() === "") {
-      alert("Por favor, ingrese una tarea.");
-      return;
-    }
+  lista.appendChild(nuevaTarea);
 
-    const lista = document.getElementById("listaTareas");
-    
-    const tituloLista = document.querySelector("#lista");
-    tituloLista.innerHTML = 'Lista de tareas'
+  document.getElementById("ingresoTarea").value = "";
+}
 
-    const nuevaTarea = document.createElement("li");
-    nuevaTarea.className = "list-group-item d-flex justify-content-between align-items-center";
-    nuevaTarea.innerHTML = `
-      ${tarea}
-      <button class="btn btn-danger btn-sm" onclick="eliminarTarea(this)">Eliminar</button>
-    `;
-
-    lista.appendChild(nuevaTarea);
-
-    document.getElementById("ingresoTarea").value = "";
-  }
-
-  function eliminarTarea(elemento) {
-    const tarea = elemento.parentNode;
-    tarea.parentNode.removeChild(tarea);
-  }
-
-  
+function eliminarTarea(elemento) {
+  const tarea = elemento.parentNode;
+  tarea.parentNode.removeChild(tarea);
+}
