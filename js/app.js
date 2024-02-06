f7e0acf28562e220023b3ef1d659504c702172ff
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2024-02-06 14:31:25 -0300

    se agrego la funcion de agregar tareas al localstorage

--- js/app.js
+++ js/app.js
@@ -11,11 +11,62 @@ function agregarTarea(event) {
   `;
 
   lista.appendChild(nuevaTarea);
-
+  guardarTareaEnLocalStorage(tarea);
   document.getElementById("ingresoTarea").value = "";
 }
 
 function eliminarTarea(elemento) {
   const tarea = elemento.parentNode;
   tarea.parentNode.removeChild(tarea);
+  eliminarTareaDelLocalStorage(tarea.firstChild.textContent.trim());
 }
+
+function guardarTareaEnLocalStorage(tarea) {
+  let tareas;
+  if (localStorage.getItem("tareas") === null) {
+    tareas = [];
+  } else {
+    tareas = JSON.parse(localStorage.getItem("tareas"));
+  }
+  tareas.push(tarea);
+  localStorage.setItem("tareas", JSON.stringify(tareas));
+}
+
+function cargarTareasDesdeLocalStorage() {
+  let tareas;
+  if (localStorage.getItem("tareas") === null) {
+    tareas = [];
+  } else {
+    tareas = JSON.parse(localStorage.getItem("tareas"));
+  }
+
+  tareas.forEach(function (tarea) {
+    const lista = document.getElementById("listaTareas");
+    const nuevaTarea = document.createElement("li");
+    nuevaTarea.className =
+      "list-group-item d-flex justify-content-between align-items-center";
+    nuevaTarea.innerHTML = `
+      ${tarea}
+      <button class="btn btn-danger btn-sm" onclick="eliminarTarea(this)">Eliminar</button>
+    `;
+    lista.appendChild(nuevaTarea);
+  });
+}
+
+function eliminarTareaDelLocalStorage(tarea) {
+  let tareas;
+  if (localStorage.getItem("tareas") === null) {
+    tareas = [];
+  } else {
+    tareas = JSON.parse(localStorage.getItem("tareas"));
+  }
+  tareas.forEach(function (tareaElement, index) {
+    if (tareaElement === tarea) {
+      tareas.splice(index, 1);
+    }
+  });
+
+  localStorage.setItem("tareas", JSON.stringify(tareas));
+}
+
+document.addEventListener("DOMContentLoaded", cargarTareasDesdeLocalStorage);
